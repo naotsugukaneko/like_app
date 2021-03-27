351cf00e9724618d1c3773bbd85ab941ee30d278
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-03-27 07:41:36 +0000

    モデルの関連付け（1対多）したアプリ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # データを全削除
+# User.destroy_all
+# Post.destroy_all
+
+# user1 = User.create!(email: "satou@example.com", password: "password")
+# user2 = User.create!(email: "suzuki@example.com", password: "password")
+# user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+# # # Post.create!(content: "おはよう", user_id: user2.id)
+# # # Post.create!(content: "こんにちは", user_id: user3.id)
+# # # Post.create!(content: "こんばんは", user_id: user3.id)
+
+# # # puts "初期データの投入に成功しました！"
+
+# user2.posts.create!(content: "おはよう")
+# user3.posts.create!(content: "こんにちは")
+# user3.posts.create!(content: "こんばんは")
+
+# puts "初期データの投入に成功しました！"
+
+EMAIL = "test@example.com"
+PASSWORD = "password"
+
+User.destroy_all
+
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+user2.posts.create!(content: "沖縄そば")
+user1.posts.create!(content: "海ぶどう")
+user3.posts.create!(content: "ゴーヤーチャンプルー")
+user3.posts.create!(content: "サーターアンダギー")
+user1.posts.create!(content: "タコライス")
+
+User.create!(email: EMAIL, password: PASSWORD)
+
+puts "初期データの投入に成功しました！"
