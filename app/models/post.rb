c1f28c10088a9f696d81acd4f8536463d14a5771
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-03-30 00:01:32 +0000

    いいね数のカウント実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+  
+   # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」
+  def liked_by?(user)
+    likes.find_by(user_id: user.id).present?
+  end
 end
